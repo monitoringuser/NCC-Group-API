167d2b0e4e34ca40ee26d24720b963868e68ba61
Eddie Jaoude <eddie@jaoudestudios.com>
2013-07-27 15:02:32 +0100

    POC of date picker, including 24hours, 7days, 1month etc (client side only)

--- public/js/default.js
+++ public/js/default.js
@@ -6,4 +6,22 @@ $(document).ready(function(){
     // tooltip
     $('.popovertitle').popover();
 
+    // date range picker
+    $('#reportrange').daterangepicker(
+        {
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                'Last 7 Days': [moment().subtract('days', 6), moment()],
+                'Last 30 Days': [moment().subtract('days', 29), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+            },
+            timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'
+        },
+        function(start, end) {
+            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+    );
+
 });
